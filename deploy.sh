414e129fc5b81bb588a9dc227362c8ce1b86c1b9
robin_8588 <robin_8588@aliyun.com>
2020-06-07 16:05:18 +0800

    add swagger

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-aws s3 cp swagger.yaml s3://aws-sam-cli-managed-default-samclisourcebucket-hfecv3sejgsr/swagger.yaml
+aws s3 cp index.html s3://shoturlweb/index.html
+aws s3 cp 404.html s3://shoturlweb/404.html
+aws s3 cp sync swaggerui/ s3://shoturlweb/swaggerui/
 sam deploy
