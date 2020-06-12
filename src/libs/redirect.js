0ef0cf242c86b51ea51b6b669d27812da59dab5a
robin_8588 <robin_8588@aliyun.com>
2020-06-12 18:55:23 +0800

    update redirect

--- src/libs/redirect.js
+++ src/libs/redirect.js
@@ -6,6 +6,7 @@ function redirect(url) {
     return {
         statusCode: 301,
         headers: {
+            "Content-Type": "text/html; charset=UTF-8",
             Location: url
         },
         body:'<html><head><title>301 Moved Permanently</title></head><body bgcolor="white"><center><h1>301 Moved Permanently</h1></center><hr><center>ShotUrl</center></body></html>'
